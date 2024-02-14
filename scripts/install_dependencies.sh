e853563acfa4d3908ae96a1907580791b8eaed3a
Adam C <xiaotcao@gmail.com>
2024-02-14 13:58:39 -0800

    Fix install dependency code

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,17 @@
 #! /bin/bash
 echo "install dependencies"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install --lts
 
-node -e "console.log('Running Node.js ' + process.version)"
+
+if command -v nvm &> /dev/null; then
+    echo "NVM is already installed."
+else
+    echo "NVM is not installed."
+    # Insert your logic here
+    if [ -d "${HOME}/.nvm/.git" ]; then echo "nvm installed"; else echo "nvm not installed"; fi
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+    . ~/.nvm/nvm.sh
+    nvm install --lts
+    node -e "console.log('Running Node.js ' + process.version)"
+fi
+
 npm install
