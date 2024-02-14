8992ed19b7098e158b0c6e5cad3430d29278bf6c
Adam C <xiaotcao@gmail.com>
2024-02-14 14:21:51 -0800

    Fixing dependency install condition check

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,12 +2,11 @@
 echo "install dependencies"
 
 
-if command -v nvm &> /dev/null; then
+if [ -d "${HOME}/.nvm/.git" ];  then
     echo "NVM is already installed."
 else
     echo "NVM is not installed."
     # Insert your logic here
-    if [ -d "${HOME}/.nvm/.git" ]; then echo "nvm installed"; else echo "nvm not installed"; fi
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
     . ~/.nvm/nvm.sh
     nvm install --lts
